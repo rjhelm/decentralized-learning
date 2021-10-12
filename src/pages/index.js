ec9a4e05382bdac7ac708ffc0695b0c39331ee66
rjhelm <rjhelm@github.io>
2021-10-12 14:29:05 -0600

    created feature page for explaining all dlrn features/projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import Layout from "../components/Layout";
 import Header from "../components/Header";
-
+import Hero from "../components/Hero";
 const Index = () => {
   return (
     <Layout pageTitle="Landing Page Nextjs">
       <Header />
+      <Hero />
     </Layout>
   )
 }
