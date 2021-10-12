dbb47c9deb70aca05b1afc1f5dc140a3f6816764
rjhelm <rjhelm@github.io>
2021-10-12 14:17:19 -0600

    created new layout

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+import Header from "../components/Header";
+
+const Index = () => {
+  return (
+    <Layout pageTitle="Landing Page Nextjs">
+      <Header />
+    </Layout>
+  )
+}
+export default Index;
